7d595c1c979a568e425b9abe2ddf8efa4ca35ced
tcordeirojr <tedcordeirojr@gmail.com>
2024-11-07 01:59:41 -0300

    minhaApresentacao

--- src/components/Eventos/Evento.jsx
+++ src/components/Eventos/Evento.jsx
@@ -1,6 +1,6 @@
 function Button (){
     function meuEvento(){
-        console.log("O evento foi ativado")  // Este é um simples console.log para demonstrar o evento de click.
+        alert("O evento foi ativado")  // Este é um simples console.log para demonstrar o evento de click.
     }
     return(
         <button onClick={meuEvento}> click aqui </button>
