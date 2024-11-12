78950ab232576cc3ba454a63ebc6e9573dde9860
tcordeirojr <tedcordeirojr@gmail.com>
2024-11-12 12:36:59 -0300

    estilizacao de elementos

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,14 @@ function App() {
     <img className='imagemPerfil' src="..//src/image/perfil.jpeg" alt="minha foto de pefil" />
     <h1 className='tituloPrincipal'> Olá, me chamo Ted! 🖖🏼</h1>
     <h2 className='tituloApresentacao'>Sou Ted Junior, apaixonado por tecnologia! Estou me especializando como desenvolvedor frontend com o objetivo de me tornar fullstack, sou formado em Analise e desenvolvimento de sistemas pela Wyden, estou cursando Inglês para acrescentar no meu desenvolvimento pessoal e profissional. Ah! Eu amo futebol e jogar video game. 🚀 </h2>
-    
+    <h1 className='habilidades'>Minhas Habilidades</h1>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
       
     </>
   )
