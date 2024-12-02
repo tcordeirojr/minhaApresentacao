164ade18144c9c811d7fba3c47cabfc117916646
tcordeirojr <tedcordeirojr@gmail.com>
2024-12-02 13:32:25 -0300

    inicio de criacao da area projetos

--- src/components/Projetos/Projetos.jsx
+++ src/components/Projetos/Projetos.jsx
@@ -4,9 +4,16 @@ function Projeto(){
 return(
   <div className='Projeto'>
     <motion.h1 animate={{x:20}}> </motion.h1>
+    <div className='Carousel'>
     <img src='https://placehold.co/250x300' alt="" />
+    <img src='https://placehold.co/250x300' alt="" />
+    <img src='https://placehold.co/250x300' alt="" />
+    <img src='https://placehold.co/250x300' alt="" />
+    </div>
   </div>  
 );
 
 }
 export default Projeto
+
+
