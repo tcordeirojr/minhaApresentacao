b58ed8b64eb1b060b0d7c502017053196fd23b96
tcordeirojr <tedcordeirojr@gmail.com>
2024-12-10 01:31:50 -0300

    finalizacao de projetos

--- src/components/Projetos/Projetos.jsx
+++ src/components/Projetos/Projetos.jsx
@@ -3,12 +3,11 @@ import { motion } from 'framer-motion'
 function Projeto(){
 return(
   <div className='Projeto'>
-    <motion.h1 animate={{x:20}}> </motion.h1>
     <div className='Carousel'>
-    <img src='https://placehold.co/250x300' alt="" />
-    <img src='https://placehold.co/250x300' alt="" />
-    <img src='https://placehold.co/250x300' alt="" />
-    <img src='https://placehold.co/250x300' alt="" />
+    <a href="https://tcordeirojr.github.io/landing-page-twd/" target="_blank"><img  className='Project' src='../src/components/Projetos/images/project01.png' alt="" /></a>
+    <a href="https://tcordeirojr.github.io/previsao-do-tempo/" target="_blank" rel="noopener noreferrer"><img  className='Project' src='../src/components/Projetos/images/project02.png' alt="" /></a>
+    <a href="https://tcordeirojr.github.io/Calculadora/" target="_blank"><img  className='Project' src='../src/components/Projetos/images/project03.png' alt="" /></a>
+    <a href="https://tcordeirojr.github.io/pokemon-card/" target='_blank'><img  className='Project' src='../src/components/Projetos/images/project04.png' alt="" /></a>
     </div>
   </div>  
 );
