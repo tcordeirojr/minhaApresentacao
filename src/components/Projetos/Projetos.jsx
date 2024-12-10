4a6d0f644548900668adb6bbe46c1c5a65b85ba1
tcordeirojr <tedcordeirojr@gmail.com>
2024-12-10 02:32:50 -0300

    modificacao de moldura de projetos

--- src/components/Projetos/Projetos.jsx
+++ src/components/Projetos/Projetos.jsx
@@ -4,10 +4,10 @@ function Projeto(){
 return(
   <div className='Projeto'>
     <div className='Carousel'>
-    <a href="https://tcordeirojr.github.io/landing-page-twd/" target="_blank"><img  className='Project' src='../src/components/Projetos/images/project01.png' alt="" /></a>
-    <a href="https://tcordeirojr.github.io/previsao-do-tempo/" target="_blank" rel="noopener noreferrer"><img  className='Project' src='../src/components/Projetos/images/project02.png' alt="" /></a>
-    <a href="https://tcordeirojr.github.io/Calculadora/" target="_blank"><img  className='Project' src='../src/components/Projetos/images/project03.png' alt="" /></a>
-    <a href="https://tcordeirojr.github.io/pokemon-card/" target='_blank'><img  className='Project' src='../src/components/Projetos/images/project04.png' alt="" /></a>
+    <a href="https://tcordeirojr.github.io/landing-page-twd/" target="_blank"><img  className='Project' src='../src/components/Projetos/images/Project1.png' alt="" /></a>
+    <a href="https://tcordeirojr.github.io/previsao-do-tempo/" target="_blank" rel="noopener noreferrer"><img  className='Project' src='../src/components/Projetos/images/Project2.png' alt="" /></a>
+    <a href="https://tcordeirojr.github.io/Calculadora/" target="_blank"><img  className='Project' src='../src/components/Projetos/images/Project3.png' alt="" /></a>
+    <a href="https://tcordeirojr.github.io/pokemon-card/" target='_blank'><img  className='Project' src='../src/components/Projetos/images/Project4.png' alt="" /></a>
     </div>
   </div>  
 );
