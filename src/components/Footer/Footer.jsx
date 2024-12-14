c05406cba8d04e5599813f582bd604d5e5f52d38
tcordeirojr <tedcordeirojr@gmail.com>
2024-12-14 16:58:25 -0300

    finalizacao de elementos footer faltando apenas linkar as redes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,27 @@
+import './Footer.css'
+
+
 function footer(){
     return(
         <>
+        <div className='icon' >
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+            <img src="../src/components/Footer/ImageFooter/WhatsappIcon.png" alt="" />
+            </a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+            <img src="../src/components/Footer/ImageFooter/InstagramIcon.png" alt="" />
+            </a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+            <img src="../src/components/Footer/ImageFooter/LinkedinIcon.png" alt="" />
+            </a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+            <img src="../src/components/Footer/ImageFooter/GithubIcon.png" alt="" />
+            </a>
+        </div>
         <div className="footer">
-            <p>Todos os direitos reservados - 2024</p>
-            <p>Desenvolvido por Ted</p>
+            <p>Todos os direitos reservados - 2024 Desenvolvido por Ted</p>
+            <p></p>
+            
         </div>
         </>
     )
