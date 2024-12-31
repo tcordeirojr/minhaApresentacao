5fdd35993d9d1f9b5ae75754a3a68fac58facbf5
tcordeirojr <tedcordeirojr@gmail.com>
2024-12-31 18:32:22 -0300

    atualizacao de app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import './index.css'
 import Projeto from './components/Projetos/Projetos'
 import Habilidades from './components/Habilidades/habilidades'
 import Presentation from './components/Presentation/Presentation'
-import Footer from './components/Footer/Footer' 
+import Social from './components/Social/Social' 
 
 function App() {
   return (
     <>
     <Presentation/>
-    <Footer/>
+    <Social/>
     <Habilidades/>
     <Projeto/>
     </>
