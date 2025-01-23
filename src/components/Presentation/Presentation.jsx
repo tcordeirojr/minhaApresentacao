d09d74a5ac7853743723daf517f0d16f0462e29b
tcordeirojr <tedcordeirojr@gmail.com>
2025-01-22 21:14:56 -0300

    centralizacao da ultima habilidade

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -2,7 +2,7 @@ import './Presentation.css'
 function Presentation(){
     return(
     <>
-    <img className='imagemPerfil' src="../src/assets/image/imagePerfil.png" alt="foto de perfil"/>
+    <img className='imagemPerfil' src="./src/assets/image/imagePerfil.png" alt="foto de perfil"/>
     <h1 className='tituloPrincipal'> Olá, me chamo Ted! 🖖🏼</h1>
     <h2 className='tituloApresentacao'>Sou de Belém do Pará, apaixonado por açaí e tecnologia! Estou me especializando como desenvolvedor frontend com o objetivo de me tornar fullstack, sou formado em Análise e desenvolvimento de sistemas pela Wyden, estou cursando Inglês para acrescentar no meu desenvolvimento pessoal e profissional. Ah! Eu amo futebol e jogar video games. 🚀 </h2>
     </>
