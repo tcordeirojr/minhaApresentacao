0f1a0efd995442bfab355dcd7569fcd623f8b0d3
tcordeirojr <tedcordeirojr@gmail.com>
2025-01-22 22:17:00 -0300

    test responsible

--- src/components/Habilidades/Habilidades.jsx
+++ src/components/Habilidades/Habilidades.jsx
@@ -1,26 +1,36 @@
 import './Habilidades.css'
-function Habilidades(){
-return (
+function Habilidades() {
+  return (
     <>
-<h1 className='habilidades'>Minhas Habilidades 🧠</h1>
-    <ul className='iconesHabilidades'>
-      <li>
-        <i className="fa-brands fa-html5"></i>
-        <h2>HTML</h2><p>Conhecimentos avançados de estruturação web  </p></li>
-      <li>
-      <i className="fa-brands fa-css3-alt"></i>
-        <h2>CSS</h2><p>Conhecimentos avançados em estilização web</p></li>
-        <li>
-        <i className="fa-brands fa-js"></i>
-        <h2>JavaScript</h2><p>Conhecimentos Avaçados em script,funcionalidades e interatividades a sites </p></li>
-      <li>
-      <i className="fa-brands fa-react"></i>
-        <h2>React</h2><p>Conhecimento Básico em criações de UI</p></li>
-      <li>
-      <i class="fa-brands fa-git-alt"></i>
-        <h2>GIT</h2><p>Conhecimentos Avançados em controles de versionamentos </p></li>
-    </ul>
+      <h1 className='habilidades'>Minhas Habilidades 🧠</h1>
+      <ul className='iconesHabilidades'>
+        <li className='unit '>
+          <i className="fa-brands fa-html5"></i>
+          <h2>HTML</h2>
+          <p>Conhecimento avançado</p>
+        </li>
+        <li className='unit '>
+          <i className="fa-brands fa-css3-alt"></i>
+          <h2>CSS</h2>
+          <p>Conhecimento avançado</p>
+        </li>
+        <li className='unit ' >
+          <i className="fa-brands fa-js"></i>
+          <h2>JavaScript</h2>
+          <p>Conhecimento avaçado</p>
+        </li>
+        <li className='unit '>
+          <i className="fa-brands fa-react"></i>
+          <h2>React</h2>
+          <p>Conhecimento intermediário</p>
+        </li>
+        <li className='unit '>
+          <i class="fa-brands fa-git-alt"></i>
+          <h2>GIT</h2>
+          <p>Conhecimento avançado</p>
+        </li>
+      </ul>
     </>
-      )
+  )
 }
 export default Habilidades
