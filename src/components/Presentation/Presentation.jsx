cef943f8b0fece39d4c0af49c88e480e75ded9af
tcordeirojr <tedcordeirojr@gmail.com>
2025-03-03 01:04:52 -0300

    ajuste portifolio

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -4,7 +4,7 @@ function Presentation(){
     <>
     <img className='imagemPerfil' src="../src/assets/image/foto.png" alt="foto de perfil"/>
     <h1 className='tituloPrincipal'> Olá, me chamo Ted! 🖖🏼</h1>
-    <h2 className='tituloApresentacao'>Sou de Belém do Pará, apaixonado por açaí e tecnologia! Estou me especializando como desenvolvedor frontend com o objetivo de me tornar fullstack, sou formado em Análise e desenvolvimento de sistemas pela Wyden, estou cursando Inglês para acrescentar no meu desenvolvimento pessoal e profissional. Ah! Eu amo futebol e jogar video games. 🚀 </h2>
+    <h2 className='tituloApresentacao'>Sou de Belém do Pará, apaixonado por açaí e tecnologia! Estou me especializando como desenvolvedor frontend com o objetivo de me tornar fullstack, sou formado em Análise e desenvolvimento de sistemas pela Wyden, estou cursando Inglês para acrescentar no meu desenvolvimento pessoal e profissional. Ah! Eu amo futebol e jogar video games.   🚀 </h2>
     </>
     )
 }
